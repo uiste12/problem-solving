f0854233fbb4542e510dace1ad006fd7f088c90a
uiste12 <anazaza025@gmail.com>
2024-01-19 23:53:42 +0100

    add another comment

--- sleep.js
+++ sleep.js
@@ -1,7 +1,7 @@
 //synchronous code
 let a = 5;
 console.log(`the value is ${a} `);
-
+// merge branch => add files => commit with cmts => optionel "pull changes from origin before pushing yours" =>  push 
 let v= "World";
 console.log(`Hello ${v}`);
 
